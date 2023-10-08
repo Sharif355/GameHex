e3f41e0a0afc98a5d290817d1fde00e3707bbeb2
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-10-08 15:32:17 +0600

    commit

--- React Authentication/event-management/src/Pages/Home/Banner.jsx
+++ React Authentication/event-management/src/Pages/Home/Banner.jsx
@@ -3,6 +3,7 @@ const Banner = () => {
     <div className="border mx-auto my-20">
       <h2 className="text-3xl">This is Banner</h2>
       <p>here all the banner items will be show</p>
+      <img src="" alt="" />
     </div>
   );
 };
