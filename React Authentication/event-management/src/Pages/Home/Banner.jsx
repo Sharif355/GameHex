26ab0ff3fe9695128ec4b6ed6a1a0f5875479f4a
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-10-08 15:27:07 +0600

    changes in banner section

--- React Authentication/event-management/src/Pages/Home/Banner.jsx
+++ React Authentication/event-management/src/Pages/Home/Banner.jsx
@@ -2,6 +2,7 @@ const Banner = () => {
   return (
     <div className="border mx-auto my-20">
       <h2 className="text-3xl">This is Banner</h2>
+      <p>here all the banner items will be show</p>
     </div>
   );
 };
